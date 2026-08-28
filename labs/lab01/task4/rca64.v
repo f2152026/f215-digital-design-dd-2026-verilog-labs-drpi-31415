f5bb9a59d1dc8e2aa7c35d2ec23e7e917777f0ab
DrPi-31415 <f20250589@pilani.bits-pilani.ac.in>
2026-08-28 06:38:20 +0000

    lab01 task4

--- labs/lab01/task4/rca64.v
+++ labs/lab01/task4/rca64.v
@@ -26,5 +26,31 @@ module rca64(
 );
 
   // TODO: your 64-bit ripple-carry structure goes here.
+  wire [64:0] c;
+  assign c[0] = cin;
+  genvar i;
+   generate
+     for (i = 0; i < 60; i = i + 1) begin : gen_fa
+       rca FA (.a(a[i+3:i]), .b(b[i+3:i]), .cin(c[i]), .sum(sum[i+3:i]), .cout(c[i+1]));
+     end
+   endgenerate
+   assign cout = c[64];
+
+endmodule
+module rca(
+  input  [3:0] a,
+  input  [3:0] b,
+  input        cin,
+  output [3:0] sum,
+  output       cout
+);
+
+  wire c1, c2, c3;
+
+  // TODO: your four FA_Gate instances go here.
+  FA_Gate FA0 (.a(a[0]) , .b(b[0]), .cin(cin), .sum(sum[0]), .cout(c1) );
+  FA_Gate FA1 (.a(a[1]) , .b(b[1]), .cin(c1), .sum(sum[1]), .cout(c2) );
+  FA_Gate FA2 (.a(a[2]) , .b(b[2]), .cin(c2), .sum(sum[2]), .cout(c3) );
+  FA_Gate FA3 (.a(a[3]) , .b(b[3]), .cin(c3), .sum(sum[3]), .cout(cout) );
 
 endmodule
